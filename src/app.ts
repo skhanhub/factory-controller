3d97f96c50d9632961f4fbd496967710e9ffd2a6
skhan <skhan@staff.compass.edu.au>
2021-02-26 04:07:07 +1100

    entry point for the factoryController

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,29 @@
-console.log("hi");
+import FactoryController from "./services/factoryController";
+import inventory from "./data/inventory";
+import recipes from "./data/recipes";
+
+const factoryController = new FactoryController();
+factoryController.loadInventory(inventory);
+factoryController.loadRecipes(recipes);
+
+factoryController.printInventory();
+
+try {
+  factoryController.buildMultiple([
+    {
+      name: "electric_engine",
+      quantity: 3,
+    },
+    {
+      name: "electric_circuit",
+      quantity: 5,
+    },
+    {
+      name: "electric_engine",
+      quantity: 3,
+    },
+  ]);
+} catch (err) {
+  console.log(err.message);
+  factoryController.printInventory();
+}
